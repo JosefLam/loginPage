26ebfbe2fb7daa278191cf82c5872079c0cdb515
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2023-02-01 12:40:19 +0000

    server created

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace Server
 {
@@ -11,6 +13,59 @@ namespace Server
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome the login server");
+
+            //create a HTTP server that listens on port 8080
+            const int port = 8080;
+            string prefix = $"http://localhost:{port}/";
+
+            Console.WriteLine("Listening");
+
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add(prefix);
+            server.Start();
+            
+
+            bool running = true;
+            while(running)
+            {
+                HttpListenerContext context = server.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                string html = $"";
+                byte[] buffer = Encoding.UTF8.GetBytes(html);
+
+                
+                Console.WriteLine($"Request:{request.RawUrl}");
+
+
+                switch(request.RawUrl) {
+                    case "/":
+                        html = File.ReadAllText($"../../static/index.html");
+                        break;
+                    default:
+                        string path = "../../static" + request.RawUrl;
+                        if (File.Exists(path))
+                        {
+                            html = File.ReadAllText(path);
+
+                        } else
+                        {
+                            response.StatusCode = 404;
+                            html = "sorry -file not found";
+                            Console.WriteLine($"Unknown Url: {request.RawUrl}");
+                        }
+                        Console.WriteLine($"UnKnown URL: {request.RawUrl}");
+                        break;
+                }
+                Console.WriteLine($"Sending: {buffer.Length} bytes");
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+                server.Stop();
+            }
+
+            Console.ReadLine();
         }
     }
 }
