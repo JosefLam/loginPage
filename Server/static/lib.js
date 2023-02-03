0c1cef4adc61f972169983b485b4193570671474
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2023-02-03 16:25:32 +0000

    using a constant for small optimisation

--- Server/static/lib.js
+++ Server/static/lib.js
@@ -1,6 +1,7 @@
 ﻿const eHints = document.getElementById("hints");
 const audio = document.getElementById("11");
 const logoEggNum = 10;
+const charError = "Must be between 5 and characters";
 var logoClickNum = 0;
 var createUserBox = false;
 
@@ -29,8 +30,8 @@ function createUser() {
         let newPassword = document.getElementById("newPassword").value
         try {
             if (newPassword == password) {
-                presenceCheck(password, "Please enter your password");
-                lengthCheck(password, 5, 10, "Must be between 5 and character");
+                presenceCheck(password, charError);
+                lengthCheck(password, 5, 10, charError);
                 eHints.innerText = "Attempting Account creation";
             } else {
                 throw "passwords do not match"
@@ -47,8 +48,8 @@ function login() {
     try {
         presenceCheck(username, "Please enter your username");
         presenceCheck(password, "Please enter your password");
-        lengthCheck(username, 5, 10, "Must be between 5 and character");
-        lengthCheck(password, 5, 10, "Must be between 5 and character");
+        lengthCheck(username, 5, 10, charError);
+        lengthCheck(password, 5, 10, charError);
         eHints.innerText = "Attempting login";
         eForm.Submit();
     } catch (e) {
